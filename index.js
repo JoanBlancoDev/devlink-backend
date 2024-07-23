94f811db940f3d2f937061442394ee86d17819d4
Joan Blanco <joanblanco.dev@gmail.com>
2024-07-23 09:55:34 -0400

    React deploy

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const path = require('path')
 
 // Init app
 const app = express();
@@ -22,5 +23,9 @@ app.use( express.json() );
 app.use('/api/auth', require('./routes/auth') );
 app.use('/api/events', require('./routes/events') );
 app.use('/api/user', require('./routes/user') );
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
 
 app.listen( process.env.PORT, () => console.log(`Server running port ${process.env.PORT}`) );
